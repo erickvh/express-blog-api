5129c0b87b9da16420f8f92a9c8e449c470a32e3
Erick Ventura <erick94111@gmail.com>
2020-05-29 11:23:46 -0600

    add middleware content type

---
+++ src/middleware/handler.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { ValidationError } from 'class-validator';
+import { generateResponse } from '../handler/response';
+
+class ClientError extends Error {
+    statusCode: number;
+    errors?: string[] | ValidationError[];
+    constructor(
+        statusCode: number,
+        message: string,
+        errors?: string[] | ValidationError[]
+    ) {
+        super();
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+    }
+}
+
+function handleErrors(
+    error: Error,
+    req: express.Request,
+    res: express.Response,
+    next: Function
+): void {
+    if (error instanceof ClientError)
+        return generateResponse(
+            res,
+            error.statusCode,
+            error.message,
+            error.errors
+        );
+    next(error);
+}
+
+export { handleErrors, ClientError };
